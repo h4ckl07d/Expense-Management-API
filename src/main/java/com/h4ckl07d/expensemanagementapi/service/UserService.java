9438f09ba9aa4244ec5d2d03a807d43b074b0633
h4ckl07d <lawalgbolahan042@gmail.com>
2026-08-15 14:56:42 +0100

    created services

--- src/main/java/com/h4ckl07d/expensemanagementapi/service/UserService.java
+++ src/main/java/com/h4ckl07d/expensemanagementapi/service/UserService.java
@@ -1,4 +1,19 @@
 package com.h4ckl07d.expensemanagementapi.service;
 
+import com.h4ckl07d.expensemanagementapi.entity.User;
+import com.h4ckl07d.expensemanagementapi.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    public UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void addUser(User user){
+        userRepository.save(user);
+    }
+
 }
