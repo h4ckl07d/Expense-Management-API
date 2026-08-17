af4d323e3fd5fa143b53ec888c7398bf9e19b4fa
h4ckl07d <lawalgbolahan042@gmail.com>
2026-08-17 14:59:57 +0100

    Completed User endpoints

--- src/main/java/com/h4ckl07d/expensemanagementapi/controller/UserController.java
+++ src/main/java/com/h4ckl07d/expensemanagementapi/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.h4ckl07d.expensemanagementapi.controller;
 
+import com.h4ckl07d.expensemanagementapi.dto.request.CreateUserRequest;
+import com.h4ckl07d.expensemanagementapi.entity.User;
+import com.h4ckl07d.expensemanagementapi.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(
+            @RequestBody CreateUserRequest request){
+        User user = userService.createUser(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(user);
+    }
+    @GetMapping("/users/{id}")
+    public User getUserById(
+            @PathVariable Long id){
+        return userService.getAllUserById(id);
+    }
 }
