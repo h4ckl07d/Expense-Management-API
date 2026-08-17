47d378cbfb8cf18333ef24c1955acf20febbf1a7
h4ckl07d <lawalgbolahan042@gmail.com>
2026-08-17 14:58:13 +0100

    Completed User Service layer

--- src/main/java/com/h4ckl07d/expensemanagementapi/service/UserService.java
+++ src/main/java/com/h4ckl07d/expensemanagementapi/service/UserService.java
@@ -1,5 +1,6 @@
 package com.h4ckl07d.expensemanagementapi.service;
 
+import com.h4ckl07d.expensemanagementapi.dto.request.CreateUserRequest;
 import com.h4ckl07d.expensemanagementapi.entity.User;
 import com.h4ckl07d.expensemanagementapi.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,21 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void addUser(User user){
-        userRepository.save(user);
+    public User createUser(CreateUserRequest request){
+        if(userRepository.existsByEmail(request.getEmail())) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+        User user = new User();
+
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+
+        return userRepository.save(user);
+    }
+
+    public User getAllUserById(Long id){
+       return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException(id + "Not found"));
     }
 
 }
