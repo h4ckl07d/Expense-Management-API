d98cbcffac263d3e5ddcbfe8f97329dd8c71d96e
h4ckl07d <lawalgbolahan042@gmail.com>
2026-08-24 15:22:56 +0100

    Added Transacion filter by parameter query

--- src/main/java/com/h4ckl07d/expensemanagementapi/service/TransactionService.java
+++ src/main/java/com/h4ckl07d/expensemanagementapi/service/TransactionService.java
@@ -3,11 +3,10 @@ package com.h4ckl07d.expensemanagementapi.service;
 import com.h4ckl07d.expensemanagementapi.dto.request.CreateTransactionRequest;
 import com.h4ckl07d.expensemanagementapi.dto.response.TransactionResponse;
 import com.h4ckl07d.expensemanagementapi.entity.Transaction;
+import com.h4ckl07d.expensemanagementapi.entity.TransactionType;
 import com.h4ckl07d.expensemanagementapi.entity.User;
 import com.h4ckl07d.expensemanagementapi.repository.TransactionRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.event.TransactionalApplicationListenerAdapter;
 
 import java.util.List;
 
@@ -39,10 +38,16 @@ public class TransactionService {
                 .orElseThrow(() -> new IllegalStateException(id + "Not Found" ) );
                 return TransactionResponse.from(transaction);
     }
-    public List<TransactionResponse> getAllTransactions(){
+    public List<TransactionResponse> getAllTransactions(User currentUser){
         List<Transaction> response = transactionRepository.findAll();
         return response.stream()
                 .map(TransactionResponse::from)
+                .toList();
+    }
+    public List<TransactionResponse> getTransactionByType(User user, TransactionType transactionType){
+        List<Transaction> response = transactionRepository.findByUserAndTransactionType(user, transactionType);
+        return response.stream()
+                .map(TransactionResponse :: from)
                 .toList();
     }
 
