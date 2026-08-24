dc63b2c9788a722f0a00b0982d244a3158ce4bb4
h4ckl07d <lawalgbolahan042@gmail.com>
2026-08-24 15:21:04 +0100

    added a method

--- src/main/java/com/h4ckl07d/expensemanagementapi/repository/TransactionRepository.java
+++ src/main/java/com/h4ckl07d/expensemanagementapi/repository/TransactionRepository.java
@@ -1,4 +1,12 @@
 package com.h4ckl07d.expensemanagementapi.repository;
 
-public interface TransactionRepository {
+import com.h4ckl07d.expensemanagementapi.entity.Transaction;
+import com.h4ckl07d.expensemanagementapi.entity.TransactionType;
+import com.h4ckl07d.expensemanagementapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByUserAndTransactionType(User user, TransactionType transactionType);
 }
