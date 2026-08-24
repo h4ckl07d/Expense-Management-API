a868e889dadb86f32cce03cd3b80295dd797d2d9
h4ckl07d <lawalgbolahan042@gmail.com>
2026-08-24 15:13:45 +0100

    added Spring Security Dependency

--- src/main/java/com/h4ckl07d/expensemanagementapi/repository/TransactionRepository.java
+++ src/main/java/com/h4ckl07d/expensemanagementapi/repository/TransactionRepository.java
@@ -1,9 +1,4 @@
 package com.h4ckl07d.expensemanagementapi.repository;
 
-import com.h4ckl07d.expensemanagementapi.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository {
 }
