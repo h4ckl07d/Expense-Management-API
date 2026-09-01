db5031e5f62899a040bfe8a5c2d5d759e610a79a
h4ckl07d <lawalgbolahan042@gmail.com>
2026-09-01 15:42:37 +0100

    Updated DTO to use record and added password field

--- src/main/java/com/h4ckl07d/expensemanagementapi/dto/request/CreateUserRequest.java
+++ src/main/java/com/h4ckl07d/expensemanagementapi/dto/request/CreateUserRequest.java
@@ -4,29 +4,16 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class CreateUserRequest {
-    @NotBlank(message = "Name is required")
-    @Size(max = 100, message = "Name must not exceed 100 characters ")
-    private String name;
+public record CreateUserRequest(
+        @NotBlank(message = "Name is required")
+        @Size(max = 100, message = "Name must not exceed 100 characters ")
+         String name,
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email is required ")
-    @Size(max =255, message = "Email must not exceed 255 characters")
-    private String email;
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email is required ")
+        @Size(max =255, message = "Email must not exceed 255 characters")
+        String email,
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-}
+        @NotBlank
+        String password
+) {}
