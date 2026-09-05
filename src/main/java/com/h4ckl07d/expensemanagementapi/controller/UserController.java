6464ca2f736f4d1578201975ba8bacf51f78ac8f
h4ckl07d <lawalgbolahan042@gmail.com>
2026-09-05 09:37:07 +0100

    Change the paths

--- src/main/java/com/h4ckl07d/expensemanagementapi/controller/UserController.java
+++ src/main/java/com/h4ckl07d/expensemanagementapi/controller/UserController.java
@@ -6,7 +6,6 @@ import com.h4ckl07d.expensemanagementapi.dto.response.UserResponse;
 import com.h4ckl07d.expensemanagementapi.entity.User;
 import com.h4ckl07d.expensemanagementapi.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.security.core.Authentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping
 public class UserController {
 
     private final UserService userService;
@@ -23,27 +22,30 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping
-    public ResponseEntity<User> createUser(
+    @PostMapping("/api/v1/auth/register")
+    public ResponseEntity<UserResponse> createUser(
             @Valid @RequestBody CreateUserRequest request){
-        User user = userService.createUser(request);
+        UserResponse response = userService.createUser(request);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(user);
+                .body(response);
     }
-    @GetMapping("/{id}")
-    public User getUserById(
+    @GetMapping("users/{id}")
+    public ResponseEntity<UserResponse> getUserById(
             @PathVariable Long id){
-        return userService.getUserById(id);
+        UserResponse response = userService.getUserById(id);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<User> updateUserDetails(
-            @PathVariable Long id,
-            @Valid @RequestBody UpdateUserRequest request, Authentication authentication
-    ){
-        User currentUser = (User) authentication.getPrincipal(); // TODO: confirm once auth is implemented
-        User updatedUser = userService.updateUser(id, request, currentUser);
-        return ResponseEntity.ok(updatedUser);
-    }
+//    @PutMapping("/{id}")
+//    public ResponseEntity<User> updateUserDetails(
+//            @PathVariable Long id,
+//            @Valid @RequestBody UpdateUserRequest request
+//    ){
+////        User currentUser = (User) authentication.getPrincipal(); // TODO: confirm once auth is implemented
+//        User updatedUser = userService.updateUser(id, request);
+//        return ResponseEntity.ok(updatedUser);
+//    }
 }
