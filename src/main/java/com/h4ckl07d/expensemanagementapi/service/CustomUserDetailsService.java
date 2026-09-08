b206f6814557940b0fb1fddad4d00c832f74de33
h4ckl07d <lawalgbolahan042@gmail.com>
2026-09-08 16:38:46 +0100

    implemented userdetail service for login

---
+++ src/main/java/com/h4ckl07d/expensemanagementapi/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.h4ckl07d.expensemanagementapi.service;
+
+import com.h4ckl07d.expensemanagementapi.entity.User;
+import com.h4ckl07d.expensemanagementapi.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User Not Found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+
+
+}
