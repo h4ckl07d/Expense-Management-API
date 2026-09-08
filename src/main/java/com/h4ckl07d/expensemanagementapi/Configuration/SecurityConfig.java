208fc412e7635809f6d195aa97846acadcfd8b83
h4ckl07d <lawalgbolahan042@gmail.com>
2026-09-08 16:30:09 +0100

    Added Authentication Manager

--- src/main/java/com/h4ckl07d/expensemanagementapi/Configuration/SecurityConfig.java
+++ src/main/java/com/h4ckl07d/expensemanagementapi/Configuration/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.h4ckl07d.expensemanagementapi.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -33,6 +35,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 
 
