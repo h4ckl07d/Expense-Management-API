0e0893c01000c078d38a4c714a066fcbb618ae61
h4ckl07d <lawalgbolahan042@gmail.com>
2026-09-08 16:35:12 +0100

    Updated the login endpoint to implement JWTtoken

--- src/main/java/com/h4ckl07d/expensemanagementapi/controller/UserController.java
+++ src/main/java/com/h4ckl07d/expensemanagementapi/controller/UserController.java
@@ -1,34 +1,55 @@
 package com.h4ckl07d.expensemanagementapi.controller;
 
 import com.h4ckl07d.expensemanagementapi.dto.request.CreateUserRequest;
-import com.h4ckl07d.expensemanagementapi.dto.request.UpdateUserRequest;
+import com.h4ckl07d.expensemanagementapi.dto.request.LoginUserRequest;
 import com.h4ckl07d.expensemanagementapi.dto.response.UserResponse;
-import com.h4ckl07d.expensemanagementapi.entity.User;
+import com.h4ckl07d.expensemanagementapi.service.JwtService;
 import com.h4ckl07d.expensemanagementapi.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1")
 public class UserController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthenticationManager authenticationManager, JwtService jwtService) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
-    @PostMapping("/api/v1/auth/register")
+    @PostMapping("/auth/register")
     public ResponseEntity<UserResponse> createUser(
             @Valid @RequestBody CreateUserRequest request){
         UserResponse response = userService.createUser(request);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    @PostMapping("/auth/login")
+    public  ResponseEntity<String> login(
+           @Valid @RequestBody LoginUserRequest request
+            ){
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                )
+        );
+        String token = jwtService.generateToken(authentication.getName());
+        return ResponseEntity.ok(token);
     }
     @GetMapping("users/{id}")
     public ResponseEntity<UserResponse> getUserById(
