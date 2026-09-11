9cbde96237c4beeb5dda6eccd9a4aaa4df30e49b
h4ckl07d <lawalgbolahan042@gmail.com>
2026-09-11 11:42:47 +0100

    added method for reading and validating token

--- src/main/java/com/h4ckl07d/expensemanagementapi/service/JwtService.java
+++ src/main/java/com/h4ckl07d/expensemanagementapi/service/JwtService.java
@@ -29,4 +29,27 @@ public class JwtService {
                 .signWith(key)
                 .compact();
     }
+
+    public String extractEmail(String token) {
+
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try{
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        }
+        catch (Exception e){
+            return false;
+        }
+    }
 }
